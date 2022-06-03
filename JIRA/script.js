a10354d5e710e20aa348f1952a2c1a772f979f6a
Sameer-Agrawal <sameerag0812@outlook.com>
2022-06-03 17:47:01 +0530

    ticket creation functionality added

--- JIRA/script.js
+++ JIRA/script.js
@@ -1,14 +1,36 @@
 let addBtn = document.querySelector(".add-btn");
-let model = document.querySelector(".model-cont");
+let modelCont = document.querySelector(".model-cont");
+let textCont = document.querySelector(".textarea-cont");
+let mainCont = document.querySelector(".main-cont");
 let addModel = true;
 
 addBtn.addEventListener("click", function(e) {
     if(addModel){
         //Show the model
-        model.style.display = "flex";
+        modelCont.style.display = "flex";
     }else{
         //Hide the model
-        model.style.display = "none";
+        modelCont.style.display = "none";
     }
     addModel = !addModel;
 });
+
+modelCont.addEventListener("keydown", function(e) {
+    let key = e.key;
+    // console.log(key);
+    if(key=='Enter'){
+        createTicket(textCont.value);
+        textCont.value = "";
+        modelCont.style.display = "none";
+        addModel = !addModel;
+    }
+})
+
+function createTicket(task){
+    let ticketCont = document.createElement("div");
+    ticketCont.setAttribute("class", "ticket-cont");
+    ticketCont.innerHTML = `<div class="ticket-color"></div>
+                            <div class="ticket-id">#qu123</div>
+                            <div class="task-area">${task}</div>`
+    mainCont.appendChild(ticketCont);
+}
