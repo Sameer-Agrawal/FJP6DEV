21d85600a4213ebc8d4b20b7fbd9833d7ec6c0b4
Sameer-Agrawal <sameerag0812@outlook.com>
2022-06-07 23:46:24 +0530

    editable cells, top-left-cell, top-row, left-col structured

---
+++ EXCEL/script.js
@@ -0,0 +1,33 @@
+let cellsContent = document.querySelector(".cells-content");
+
+cellCreator();
+
+
+
+function cellCreator(){
+    let structure = "<div class='top-left-cell'></div>";
+    // Top-row
+    structure += "<div class='top-row'>"
+    for(let count=0; count<26; count++){
+        structure += `<div class='top-row-cells'>${String.fromCharCode(65+count)}</div>`
+    }
+    structure += "</div>"
+    // Left-col
+    structure += "<div class='left-col'>"
+    for(let count=0; count<100; count++){
+        structure += `<div class='left-col-cells'>${count+1}</div>`
+    }
+    structure += "</div>"
+    // Excel-cells
+    structure += "<div class='editable-cells'>"
+    for(let row=0; row<100; row++){  // Creating 100 rows
+        structure += "<div class='row'>";
+        for(let col=0; col<26; col++){  // Creating 100 columns
+            structure += "<div class='cell' contentEditable>CELLS</div>"
+        }
+        structure += "</div>";
+    }
+    structure += "</div>"
+    cellsContent.innerHTML = structure;
+}
+
