0e553ee566de73af3ebd041eee2a2bb20deea566
Sameer-Agrawal <sameerag0812@outlook.com>
2022-06-16 09:44:34 +0530

    constructor function

--- Module4/constructorFxn.js
+++ Module4/constructorFxn.js
@@ -1,13 +1,30 @@
+// function car(Name, Model, Color){
+//     this.name = Name;
+//     this.model = Model;
+//     this.color = Color;
+
+//     this.display = function(){
+//         console.log("Hi I am driving ",this.model)
+//     }
+// }
+
+// let car1 = new car("maruti", "suzuki", "red");
+// console.log(car1)
+// car1.display();
+
+
 function car(Name, Model, Color){
-    this.name = Name;
+    this.name = Name;   //here this is pointing towards an empty object
     this.model = Model;
     this.color = Color;
-
-    this.display = function(){
-        console.log("Hi I am driving ",this.model)
+    this.test = function(){
+        console.log(`Hi I am driving "${this.model}`);
     }
+    // console.log(this);
 }
 
-let car1 = new car("maruti", "suzuki", "red");
-console.log(car1)
-car1.display();
+let car1 = new car("maruti", "suzuki", "black") 
+console.log(car1);
+
+// This constructor function is used to make similar type of objects effectively
+
